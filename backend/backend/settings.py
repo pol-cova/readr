96c6fa66f9630740d2d695e5904009d658cfa2fa
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-11-16 21:54:43 -0600

    Update CORS allowed origins and add form functionality to Login page

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CORS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:5173",]
+    "http://127.0.0.1:5173",]
 
 # Rest Framework
 REST_FRAMEWORK = {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,60 @@
 // Login page
 // Import custom components from reac-bootstrap
 import { Button, Form, Card } from 'react-bootstrap';
+// Import useState hook
+import { useState } from 'react';   
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });           
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/auth/jwt/create/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData)
+            });
+            if (response.ok) {
+                const { access, refresh } = await response.json();
+                sessionStorage.setItem('access', access);
+                sessionStorage.setItem('refresh', refresh);
+                // Reset the form
+                setFormData({
+                    username: '',
+                    password: '',
+                });
+                console.log('Data sent to the api');
+            } else {
+                console.log('Data not sent to the api');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
     <div className='d-flex justify-content-center align-items-center vh-100 bg-dark text-light'>
     <Card className='shadow-lg' style={{ width: '400px' }}>
         <Card.Body>
             <Card.Title className="mb-4 text-center" style={{ fontSize: '36px' }}>Login</Card.Title>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="username">
                     <Form.Label style={{ fontSize: '18px' }}>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your username" />
+                    <Form.Control name="username" type="text" placeholder="Enter your username" value={formData.username} onChange={handleChange} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="password">
                     <Form.Label style={{ fontSize: '18px' }}>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter your password" />
+                    <Form.Control name="password" type="password" placeholder="Enter your password" value={formData.password} onChange={handleChange} />
                 </Form.Group>
                 <Button variant="dark" type="submit" className="w-100" style={{ fontSize: '18px' }}>
                     Login
